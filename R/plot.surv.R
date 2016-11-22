37eeb7b2ff7da141bedd0d94691f5e832d521471
Marta Sestelo <sestelo@uvigo.es>
2016-11-22 18:23:39 +0100

    new arguments in the function

--- R/plot.surv.R
+++ R/plot.surv.R
@@ -1,5 +1,6 @@
 plot.surv <-
-  function(x = object, y = NULL, type = NULL, ...) {
+  function(x = object, y = NULL, conf = NULL,  type = NULL, conftype = NULL,
+           col = 1, confcol = 2, lty = 1, conflty = 2, ...) {
 
     object <- x
 
@@ -13,16 +14,37 @@ plot.surv <-
 
     ob <- object$est
 
-    if(is.null(type)){
-      type <- "l"
+    if (is.null(type)) type <- "l"
+    if (is.null(conftype)) conftype <- "l"
+
+
+    if (is.null(conf)){
+      ci <- object$conf
+    }else{
+      if (conf == TRUE & object$conf == FALSE){
+        stop("The surv object does not contain confidence intervals")
+      }
+      if (conf == FALSE) ci <- FALSE
     }
 
-    if (object$conf == FALSE) { plot(ob[, 1], ob[, 2], type = type, ...) }
 
-    if (object$conf == TRUE) {
-      matplot(x = ob[, 1], y = ob[, 2:4], type = type, ...)
-      #oask <- devAskNewPage(TRUE)
-      #on.exit(devAskNewPage(oask))
-    }
+      plot(ob[, 1], ob[, 2], type = type, col = col,...)
+      if (ci == TRUE){
+        lines(x = ob[, 1], y = ob[, 3], type = conftype, lty = conflty,
+              col = confcol, ...)
+        lines(x = ob[, 1], y = ob[, 4], type = conftype, lty = conflty,
+              col = confcol, ...)
+      }
+
+
+    #
+    #
+    # if (is.null(conf) & object$conf == FALSE) { plot(ob[, 1], ob[, 2], type = type, ...) }
+    #
+    # if (is.null(conf) & object$conf == TRUE) {
+    #   matplot(x = ob[, 1], y = ob[, 2:4], type = type, ...)
+    #   #oask <- devAskNewPage(TRUE)
+    #   #on.exit(devAskNewPage(oask))
+    # }
 
   }
