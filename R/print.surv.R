3255d880fa25f40c2c0a4a876000975a6990861d
Marta Sestelo <sestelo@uvigo.es>
2016-12-02 22:27:25 +0100

    method long

--- R/print.surv.R
+++ R/print.surv.R
@@ -4,7 +4,13 @@ print.surv <- function(x, ...){
   cat("Call:\n")
   print(x$call)
   cat("\nMethod:\n")
-  print(class(x)[1])
+
+  if(class(x)[1] == "KMW") method <- "Kaplan-Meier weights"
+  if(class(x)[1] == "LDM") method <- "Landmark approach"
+  if(class(x)[1] == "PLDM") method <- "Presmoothed Landmark approach"
+  if(class(x)[1] == "IPCW") method <- "Inverse Probability of Censoring Weighting"
+
+  print(method)
 
   }else{
     stop("Argument x must be either surv object.")
